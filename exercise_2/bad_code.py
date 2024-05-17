7782d366abf26d43e6a5b62cbdd9fba4e17eb800
willemcossey kbc <willem.cossey@student.kuleuven.be>
2024-05-17 08:20:59 +0000

    apply black to bad code

--- exercise_2/bad_code.py
+++ exercise_2/bad_code.py
@@ -1,11 +1,11 @@
 import os
 
+
 def basic_math():
-   return 2 + 2
+    return 2 + 2
 
 
 # A comment that is so long shorter.
-if __name__== "__main__":
-   hello = "Hello, World!"
-   basic_math()
-
+if __name__ == "__main__":
+    hello = "Hello, World!"
+    basic_math()
