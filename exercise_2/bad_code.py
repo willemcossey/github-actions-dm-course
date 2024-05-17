3d2d37a45d0c13b7aa758e5ae77d1f9eb0423b40
willemcossey kbc <willem.cossey@student.kuleuven.be>
2024-05-17 08:18:21 +0000

    fix problems in the code

--- exercise_2/bad_code.py
+++ exercise_2/bad_code.py
@@ -4,7 +4,7 @@ def basic_math():
    return 2 + 2
 
 
-# A comment that is so long, it is going to be split into multiple lines due for general readability purposes
+# A comment that is so long shorter.
 if __name__== "__main__":
    hello = "Hello, World!"
    basic_math()
